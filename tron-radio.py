442eefdcacd8f39b0b1ed4185ef560165fab0bd3
basti <spmbot123@gmail.com>
2015-09-25 17:04:58 +0200

    cleanups

--- tron-radio.py
+++ tron-radio.py
@@ -89,8 +89,8 @@ playlist = " "
 #functions
 def internet_on():
     try:
-        response=urllib2.urlopen('http://74.125.228.100',timeout=1)
-        return True
+       response=urllib2.urlopen('http://74.125.228.100',timeout=1)
+       return True
     except urllib2.URLError as err: pass
     return False
 
